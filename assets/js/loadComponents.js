f40db612945d3a29c45f1a2aecdbaf2526b042b2
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-09-03 23:02:57 -0300

    refactor: componentize header and footer

---
+++ assets/js/loadComponents.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+	fetch('../../components/header.html')
+	.then(res => res.text())
+	.then(data => {
+		const header = document.getElementById('header');
+		header.innerHTML = data;
+
+		initMenu();
+	})
+	.catch(err => console.error('Erro ao carregar header:', err));
+
+    fetch('../../components/footer.html')
+		.then(res => res.text())
+		.then(data => { document.getElementById('footer').innerHTML = data; })
+		.catch(err => console.error('Erro ao carregar footer:', err));
+});
